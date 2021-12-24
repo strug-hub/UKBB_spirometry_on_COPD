105aadf64baf2949aec3fa0b24548e022982fe12
Naim Panjwani <naim.panjwani@gmail.com>
2021-12-23 20:02:23 -0500

    updated with finalized results

--- locusfocus_prep/run.sh
+++ locusfocus_prep/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python merge_and_convert_to_html.py filelist_v3.txt 1:205780000-205940000 spirometry_in_ukbb_v3
+python merge_and_convert_to_html.py filelist.txt 1:205780000-205940000 spirometry_in_ukbb
